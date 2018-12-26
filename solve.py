a7962cca3db46a44439f1a1989c49e4c830d3dad
Albert Wang <git@albertyw.com>
2018-12-26 17:08:46 +0800

    Finish backtracking and add bugfixes

--- solve.py
+++ solve.py
@@ -11,17 +11,6 @@ xx59xxxx7
 2x9x53xx4
 xxxx4x9xx
 """
-matrix = """
-9x3x1xxxx
-4xx56x2x9
-7xxxx814x
-x841xxx93
-xx18394xx
-39xxx4x81
-x459xx3x7
-2x9x53xx4
-x3xx4x9xx
-"""
 answer = """
 953412678
 418567239
@@ -45,24 +34,35 @@ squares = {
     7: range(6, 9),
     8: range(6, 9),
 }
-grid = []
 tries = []
 
-row = []
-for i in matrix:
-    if not i.strip():
-        continue
-    if i != 'x':
-        i = int(i)
-    row.append(i)
-    if len(row) == 9:
-        grid.append(row)
-        row = []
+def parse_matrix(matrix):
+    grid = []
+    row = []
+    for i in matrix:
+        if not i.strip():
+            continue
+        if i != 'x':
+            i = int(i)
+        row.append(i)
+        if len(row) == 9:
+            grid.append(row)
+            row = []
+    return grid
 
 
 def print_grid(grid):
     for x in range(9):
         print(''.join([str(x) for x in grid[x]]))
+
+
+def hash_grid(grid):
+    total = 0
+    for x in range(9):
+        for y in range(9):
+            if grid[x][y] != 'x':
+                total += int(grid[x][y])
+    return total
 
 
 def finished(grid):
@@ -82,18 +82,25 @@ def add_try(grid, try_count=0):
             possibilities = compute(grid, x, y)
             while possibilities and try_count > 0:
                 possibilities = possibilities[1:]
-                try_count += 1
-            if try_count > 0:
+                try_count -= 1
+            if len(possibilities) == 0:
                 continue
             new_grid = copy.deepcopy(grid)
             new_grid[x][y] = possibilities[0]
             tries.append([grid, orig_try_count])
             return new_grid
+    raise RuntimeError("Cannot add_try")
 
 
 def revert_try():
-    old_grid, try_count = tries.pop()
-    return add_try(old_grid, try_count=try_count+1)
+    global tries
+    old_grid, try_count = tries[0]
+    tries = tries[1:]
+    try:
+        new_grid = add_try(old_grid, try_count=try_count+1)
+        return new_grid
+    except RuntimeError:
+        return revert_try()
 
 
 def compute(grid, cell_x, cell_y):
@@ -120,25 +127,35 @@ def compute(grid, cell_x, cell_y):
                 continue
             # print(grid[x][y])
             cell_possibilities.remove(grid[x][y])
-    if len(cell_possibilities) == 1:
-        print('confirmed: ', cell_possibilities[0])
     return cell_possibilities
 
 
-while not finished(grid):
-    found = False
-    for x in range(9):
-        for y in range(9):
-            if grid[x][y] != 'x':
-                continue
-            answer = compute(grid, x, y)
-            if len(answer) == 0:
-                grid = revert_try()
-            if len(answer) == 1:
-                found = True
-                grid[x][y] = answer[0]
-    if not found:
-        grid = add_try(grid)
-        print_grid(grid)
-        # raise Exception("cannot advance")
-print_grid(grid)
+def search(grid):
+    while not finished(grid):
+        found = False
+        reset = False
+        for x in range(9):
+            for y in range(9):
+                # print_grid(grid)
+                if grid[x][y] != 'x':
+                    continue
+                cell_possibilities = compute(grid, x, y)
+                if len(cell_possibilities) == 0:
+                    reset = True
+                    break
+                if len(cell_possibilities) == 1:
+                    found = True
+                    grid[x][y] = cell_possibilities[0]
+            if reset:
+                break
+        if reset:
+            grid = revert_try()
+            continue
+        if not found:
+            grid = add_try(grid)
+            # print_grid(grid)
+            # raise Exception("cannot advance")
+    print_grid(grid)
+
+
+search(parse_matrix(matrix))
