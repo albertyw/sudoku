d0d7db4474ccda0c37c62dc74f952f63a7829e5e
Albert Wang <git@albertyw.com>
2019-02-09 19:52:36 -0800

    Add a hidden singles test case

--- test.py
+++ test.py
@@ -10,3 +10,10 @@ class TestSolve(unittest.TestCase):
         solved = solve.Solver(problem).search()
         solved = solved.replace("\n", "")
         self.assertEqual(solved, solution)
+
+    def test_hidden_singles(self):
+        problem = "xx2x3xxx8xxxxx8xxxx31x2xxxxx6xx5x27xx1xxxxx5x2x4x6xx31xxxx8x6x5xxxxxxx13xx531x4xx"
+        solution = "672435198549178362831629547368951274917243856254867931193784625486592713725316489"
+        solved = solve.Solver(problem).search()
+        solved = solved.replace("\n", "")
+        self.assertEqual(solved, solution)
